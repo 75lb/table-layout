1d1813989aa8e419958f1e1343b9c0e8794633dc
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-06-27 22:48:56 +0100

    cli --width option

--- bin/cli.js
+++ bin/cli.js
@@ -1,10 +1,29 @@
 #!/usr/bin/env node
 "use strict";
-var columns = require("../");
+var columnLayout = require("../");
 var tr = require("transform-tools");
+var cliArgs = require("command-line-args");
+
+var options = cliArgs([
+    { name: "width", type: Array, alias: "w" }
+]).parse();
+
+var columns = [];
+if (options.width){
+    options.width.forEach(function(columnWidth){
+        var split = columnWidth.split(":").map(function(item){
+            return item.trim();
+        });
+        if (split[0] && split[1]){
+            columns.push({ name: split[0], width: Number(split[1]) });
+        }
+    })
+}
 
 process.stdin
     .pipe(tr.collectJson({ through: function(json){
-        return columns(json, { viewWidth: process.stdout.columns });
+        var clOptions = { viewWidth: process.stdout.columns };
+        if (columns.length) clOptions.columns = columns;
+        return columnLayout(json, clOptions);
     }}))
     .pipe(process.stdout);
