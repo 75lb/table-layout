2aaee762d1440fcad51b0593d0bd160debfd0e36
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-10-24 21:20:26 +0100

    updated tests

--- test/columnLayout.js
+++ test/columnLayout.js
@@ -3,33 +3,24 @@ var test = require('tape')
 var columnLayout = require('../')
 
 test('columnLayout(data, options)', function (t) {
-  var data = [
-    { one: 'row 1 column one .. .. ..', two: 'r1 c2' },
-    { one: 'r2 c1', two: 'row two column 2' }
-  ]
-
-  t.strictEqual(
-    columnLayout(data, { viewWidth: 40 }),
-    ' row 1 column one    r1 c2              \n .. .. ..                               \n r2 c1               row two column 2   \n'
-  )
-
+  var fixture = require('./fixture/simple-viewWidth')
+  var result =
+'<row 1 column one .. ><r1 c2           >\n\
+<.. ..               ><                >\n\
+<r2 c1               ><row two column 2>\n\
+'
+  t.strictEqual(columnLayout(fixture.data, fixture.options), result)
   t.end()
 })
 
 test('columnLayout.lines(data, options)', function (t) {
-  var data = [
-    { one: 'row 1 column one .. .. ..', two: 'r1 c2' },
-    { one: 'r2 c1', two: 'row two column 2' }
+  var fixture = require('./fixture/simple-viewWidth')
+  var result = [
+    "<row 1 column one .. ><r1 c2           >",
+    "<.. ..               ><                >",
+    "<r2 c1               ><row two column 2>"
   ]
 
-  t.deepEqual(
-    columnLayout.lines(data, { viewWidth: 40 }),
-    [
-      ' row 1 column one    r1 c2              ',
-      ' .. .. ..                               ',
-      ' r2 c1               row two column 2   '
-    ]
-  )
-
+  t.deepEqual(columnLayout.lines(fixture.data, fixture.options), result)
   t.end()
 })
--- test/rows.js
+++ test/rows.js
@@ -1,9 +1,7 @@
 'use strict'
 var test = require('tape')
 var columnLayout = require('../')
-var Table = require('../lib/table')
 var Rows = require('../lib/rows')
-var Columns = require('../lib/columns')
 
 var fixture = {
   rows: [
@@ -26,54 +24,6 @@ var fixture = {
   ]
 }
 
-test('Table summary', t => {
-  var data = [
-    { one: 'row 1 column one .. .. ..', two: 'r1 c2' },
-    { one: 'r2 c1', two: 'row two column 2' }
-  ]
-  var table = new Table(data, {
-    padding: { left: '<', right: '>' }
-  })
-  t.strictEqual(table.rows.length, 2)
-  t.strictEqual(table.columns.length, 2)
-
-  t.deepEqual(table.getWrapped(), [
-    [ ['row 1 column one .. .. ..'], ['r1 c2'] ],
-    [ ['r2 c1'], ['row two column 2'] ]
-  ])
-
-  t.deepEqual(table.getLines(), [
-    [ 'row 1 column one .. .. ..', 'r1 c2' ],
-    [ 'r2 c1', 'row two column 2' ]
-  ])
-
-  table.columns.get('one').width = 10
-
-  t.deepEqual(table.getWrapped(), [
-    [ [ 'row 1', 'column', 'one ..', '.. ..' ], [ 'r1 c2' ] ],
-    [ [ 'r2 c1' ], [ 'row two column 2' ] ]
-  ], 'table.getWrapped()')
-
-  t.deepEqual(table.getLines(), [
-    [ 'row 1', 'r1 c2' ],
-    [ 'column', null ],
-    [ 'one ..', null ],
-    [ '.. ..', null ],
-    [ 'r2 c1', 'row two column 2' ]
-  ], 'table.getLines()')
-
-  t.deepEqual(table.renderLines(), [
-    '<row 1   ><r1c2            >',
-    '<column  ><                >',
-    '<one ..  ><                >',
-    '<.. ..   ><                >',
-    '<r2 c1   ><row two column 2>'
-  ], 'table.renderLines()')
-
-  t.end()
-
-})
-
 test('contentColumns', function(t){
   var data = [
     { one: 'row 1 column one .. .. ..', two: 'r1 c2' },
@@ -84,24 +34,6 @@ test('contentColumns', function(t){
     { name: 'one', contentWidth: 25 },
     { name: 'two', contentWidth: 16 }
   ])
-  t.end()
-})
-
-test.skip('new Table(rows) sets default columns', function (t) {
-  var data = [
-    { one: 'r1c1 r1c1 r1c1 r1c1', two: 'r1c2' },
-    { one: 'r2c1 r2c1 r2c1 r2c1', two: 'r2c2' }
-  ]
-  var table = new Table(data)
-  t.strictEqual(table.rows.length, 2)
-  t.strictEqual(table.columns.length, 2)
-
-  t.deepEqual(table.columns, [
-    { name: 'one', width: 30 },
-    { name: 'two', width: 30 }
-  ])
-
-  t.deepEqual(table.rows, data)
   t.end()
 })
 
@@ -236,99 +168,5 @@ test("rows.getLines() 2", function(t){
     [ 'r2 c1 ', 'row two column 2' ]
   ])
 
-  t.end()
-})
-
-test('new Columns(columns)', function (t) {
-  var columns = new Columns(fixture.columns)
-
-  t.deepEqual(columns, fixture.columns)
-  t.end()
-})
-
-test('columns.fromRows(rows)', function (t) {
-  var columns = new Columns(fixture.columns)
-  t.deepEqual(columns, fixture.columns)
-
-  columns.fromRows(fixture.rows)
-  t.deepEqual(columns, [
-    { name: 'one', width: 6 },
-    { name: 'two', width: 4 }
-  ])
-  t.end()
-})
-
-test('columns.fromRows(rows) 5', function (t) {
-  var columns = new Columns()
-
-  columns.fromRows(fixture.rows5)
-  t.deepEqual(columns, [
-    { name: 'one', width: 5 },
-    { name: 'two', width: 49 },
-    { name: 'three', width: 40 }
-  ])
-  t.end()
-})
-
-test('columns.fromRows(rows) 5, with viewWidth', function (t) {
-  var columns = new Columns({ viewWidth: 50 })
-
-  columns.fromRows(fixture.rows5)
-  t.deepEqual(columns, [
-    { name: 'one', width: 5 },
-    { name: 'two', width: 5 },
-    { name: 'three', width: 40 }
-  ])
-  t.end()
-})
-
-test('columns.fromRows(rows), with viewWidth, 2 wrappable columns', function (t) {
-  var columns = new Columns({ viewWidth: 10 })
-
-  var rows = [
-    {
-      one: "one four seven",
-      two: "four seven eleven"
-    }
-  ]
-  columns.fromRows(rows)
-  t.deepEqual(columns, [
-    { name: 'one', width: 5 },
-    { name: 'two', width: 5 }
-  ])
-  t.end()
-})
-
-test('columns.fromRows(rows), with viewWidth, 1 wrappable columns', function (t) {
-  var columns = new Columns({ viewWidth: 20 })
-
-  var rows = [
-    {
-      one: "one four seven eight",
-      two: ".............."
-    }
-  ]
-  columns.fromRows(rows)
-  t.deepEqual(columns, [
-    { name: 'one', width: 6 },
-    { name: 'two', width: 14 }
-  ])
-  t.end()
-})
-
-test.skip('cell.getWidth()', function(t){
-  var cell = new Cell('one one one')
-  t.strictEqual(cell.getWidth(), 11)
-  t.end()
-})
-
-test.skip('cell.wrap()', function(t){
-  var options = {
-    width: 3
-  }
-  var cell = new Cell('one one one', options)
-  t.deepEqual(cell.wrap(), [
-    'one', 'one', 'one'
-  ])
   t.end()
 })
