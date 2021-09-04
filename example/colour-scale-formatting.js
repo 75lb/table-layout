21e536065af0917a48f8e6bb5468c3c2df40cdc0
Lloyd Brookes <lloyd.brookes@gmail.com>
2021-09-04 17:51:47 +0100

    add colour-scale-formatting example

---
+++ example/colour-scale-formatting.js
@@ -0,0 +1,37 @@
+import Table from 'table-layout'
+import { promises as fs } from 'fs'
+import ansi from 'ansi-escape-sequences'
+
+const rows = JSON.parse(await fs.readFile('./example/data/stock-prices.json', 'utf8'))
+const maxValue = Math.max(...rows.map(r => r.return))
+const minValue = Math.min(...rows.map(r => r.return))
+
+function getColour (value) {
+  const ratio = value >= 0 ? value / maxValue : value / minValue
+  return Math.round((100 * ratio) + 155)
+}
+
+/* solved via language - user has control */
+{
+  const proxy = rows.map(row => {
+    return new Proxy(row, {
+      get (target, property, receiver) {
+        if (property === 'return') {
+          if (target.return >= 0) {
+            return ansi.format(target.return + '%', `fg-rgb(0, ${getColour(target.return)}, 0)`)
+          } else {
+            return ansi.format(target.return + '%', `fg-rgb(${getColour(target.return)}, 0, 0)`)
+          }
+        } else {
+          return Reflect.get(target, property, receiver)
+        }
+      }
+    })
+  })
+  const table = new Table(proxy, {
+    maxWidth: 60
+  })
+
+  console.log(table.toString())
+}
+
